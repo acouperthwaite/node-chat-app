23713009090f246170f6e54e83340c6bcd1e3a4a
acouperthwaite <38387463+acouperthwaite@users.noreply.github.com>
2018-05-18 15:50:39 -0600

    Started to Add acknowledgements to socket.io calls

--- public/js/index.js
+++ public/js/index.js
@@ -2,26 +2,19 @@ var socket = io();
 
 socket.on('connect',function () {
   console.log('Connected to Server');
-
-  // socket.emit('createEmail',{
-  //   to: 'jen@example.com',
-  //   text: 'Hey, this is Andrew.'
-  // });
-
-//   socket.emit('createMessage',{
-//     from: "andrew",
-//     text: "Some Text for Created Message"
-//   });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-// socket.on('newEmail', function(email) {
-//   console.log('New email', email);
-// });
-
 socket.on('newMessage', function(message){
   console.log('newMessage', message);
+});
+
+socket.emit('createMessage',{
+  from:'Frank',
+  text: 'Hi'
+}, function (data){
+  console.log('Got it', data);
 });
--- server/server.js
+++ server/server.js
@@ -15,26 +15,20 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
-
+//socket.emit sends a message to only that socket
+//io.emit send an event to all connections
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
-  // socket.emit from admin text - welcome to the chat app
   socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
-  //socket.broadcast from admin - new user joined
   socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
-  socket.on('createMessage',(message)=>{
+  socket.on('createMessage',(message, callback)=>{
     console.log('createdMessage', message);
-//socket.emit sends a message to only that socket
-//io.emit send an event to all connections
+
     io.emit('newMessage',generateMessage(message.from, message.text));
-    //broadcast will send to all connections except the one sending it
-    // socket.broadcast.emit('newMessage',{
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // })
+
+    callback('This is from the server');
   });
 
   socket.on('disconnect',()=>{
